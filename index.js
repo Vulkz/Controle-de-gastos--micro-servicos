afd082c3a6c3e0887a5b7abdf9390f2d39864e2a
Gabriel Costa <77176879+Vulkz@users.noreply.github.com>
2025-09-15 23:10:26 +0000

    adicionado validacoes nos campos de senha e email

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function validarCampos() {
-    const emailValido = eUmEmailValido();
-    document.getElementById('botao-recuperar-senha').disabled = !emailValido;
+function mudarEmail() {
+    alterarBotoesDesabilitados();
+    alterarErrosDeEmail();
+}
 
-    const senhaValida = eUmaSenhaValida();
-    document.getElementById('botao-de-login').disabled = !emailValido || !senhaValida;
+function mudarSenha() {
+    alterarBotoesDesabilitados();
+    alterarErroDeSenha();
 }
 
 function eUmEmailValido() {
@@ -13,6 +15,39 @@ function eUmEmailValido() {
         return false;
     }
     return validarEmail(email);
+}
+
+function alterarErrosDeEmail() {
+   const email = document.getElementById('email').value;
+   if (!email) {
+        document.getElementById('erro-email-obrigatorio').style.display = "block";
+   } else {
+        document.getElementById('erro-email-obrigatorio').style.display = "none";
+   }
+
+   if (eUmEmailValido(email)) {
+        document.getElementById('erro-email-invalido').style.display = "none";
+   } else {
+        document.getElementById('erro-email-invalido').style.display = "block";
+   }
+}
+
+function alterarErroDeSenha() {
+    const password = document.getElementById('senha').value;
+
+    if (!password) {
+        document.getElementById('erro-senha-obrigatoria').style.display = "block";
+    } else {
+        document.getElementById('erro-senha-obrigatoria').style.display = "none";
+    }
+}
+
+function alterarBotoesDesabilitados() {
+    const emailValido = eUmEmailValido();
+    document.getElementById('botao-recuperar-senha').disabled = !emailValido;
+
+    const senhaValida = eUmaSenhaValida();
+    document.getElementById('botao-de-login').disabled = !emailValido || !senhaValida;
 }
 
 function eUmaSenhaValida() {
